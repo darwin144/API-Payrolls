80ae4d9bad15289c4254eecada76c7535e395cfc
Tony Aji <tonyajipalguno19@gmail.com>
2023-06-25 19:51:57 +0700

    UI pertama

---
+++ Client/Controllers/ManagerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    /*    [Authorize(Roles = "Manager")]
+    */
+    [AllowAnonymous]
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        } 
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
